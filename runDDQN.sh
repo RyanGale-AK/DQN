542a321fd5aede90843ed76f708fb262776467e7
Brandon Kates <bjk224@cornell.edu>
2020-02-14 18:25:27 -0500

    moved run scripts

--- runDDQN.sh
+++
@@ -1,1 +0,0 @@
-python -u main.py --episodes $1 --model DDQN --env $2 > ./log/ddqn$2.out 2> ./log/ddqn$2.err &
--- runQN.sh
+++
@@ -1,1 +0,0 @@
-python -u main.py --episodes $3 --model $1 --env $2 > ./log/$1$2.out 2> ./log/$1$2.err &
